0d2780ecc1649cc747b000786f7ddcc9a4fb17bb
mxotester <77177699+mxotester@users.noreply.github.com>
2021-08-29 17:04:33 +0200

    Update Demo.java

--- introduction/src/introduction/Demo.java
+++ introduction/src/introduction/Demo.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub in selenium
 
   
 		
